b6408ab70009f416e7ea154334e78c6c3b1bb7c0
Willem Meints <willem@mein-design.nl>
2015-03-27 22:24:53 +0100

    Fix navbar highlighting

---
+++ Frontend/app/scripts/controllers/navbar.js
@@ -0,0 +1,13 @@
+'use strict';
+
+function NavbarCtrl($location) {
+  var vm = this;
+
+  vm.isActive = function(location) {
+    return location === $location.path();
+  };
+}
+
+NavbarCtrl.$inject = ['$location'];
+
+angular.module('cloudyBooksApp').controller('NavbarCtrl', NavbarCtrl);
